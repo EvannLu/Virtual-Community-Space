a21f51723f6314bebfe4aa9c983122d2f7966669
EvannLu <evan.lu60@myhunter.cuny.edu>
2025-10-06 01:14:39 -0400

    event data

---
+++ server/data/events.js
@@ -0,0 +1,59 @@
+let eventsData = [
+  {
+    "EventID": 1,
+    "EventName": "+–=÷× Tour",
+    "ArtistName": "Ed Sheeran",
+    "EventDate": "2025-11-15",
+    "StartTime": "19:30:00",
+    "VenueName": "O2 Arena",
+    "City": "London",
+    "ImageURL": "./images/ed.webp",
+    "Description": "Global pop star performs his chart-topping hits live."
+  },
+  {
+    "EventID": 2,
+    "EventName": "Carnival Tour",
+    "ArtistName": "Jay Chou",
+    "EventDate": "2025-12-08",
+    "StartTime": "20:00:00",
+    "VenueName": "O2 Arena",
+    "City": "London",
+    "ImageURL": "./images/jay.webp",
+    "Description": "Mandarin pop icon presents his latest world tour."
+  },
+  {
+    "EventID": 3,
+    "EventName": "Jazz Night",
+    "ArtistName": "Esperanza Spalding",
+    "EventDate": "2026-01-20",
+    "StartTime": "18:30:00",
+    "VenueName": "The Blue Note",
+    "City": "New York, NY",
+    "ImageURL": "./images/esp.jpg",
+    "Description": "An intimate evening of acoustic jazz and vocals."
+  },
+  {
+    "EventID": 4,
+    "EventName": "The Future is Now",
+    "ArtistName": "Dua Lipa",
+    "EventDate": "2025-10-28",
+    "StartTime": "21:00:00",
+    "VenueName": "The Blue Note",
+    "City": "New York, NY",
+    "ImageURL": "./images/dua.jpg",
+    "Description": "High-energy performance featuring tracks from her new album."
+  },
+  {
+    "EventID": 5,
+    "EventName": "Rock Legends",
+    "ArtistName": "Foo Fighters",
+    "EventDate": "2026-02-14",
+    "StartTime": "19:00:00",
+    "VenueName": "The Forum",
+    "City": "Inglewood, CA",
+    "ImageURL": "./images/foo.jpg",
+    "Description": "An iconic rock concert on Valentine's Day."
+  }
+];
+
+export default eventsData;
